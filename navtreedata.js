ad94de10c054c6a6caa743150e46fba241dc71fb
vzahradnik <vzahradnik@users.noreply.github.com>
2024-06-25 14:15:39 +0000

    deploy: 8dceac3db55d5aa7251e5dd4992d6634a8537c01

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "SimpleRelay", "index.html", [
+  [ "RFID reader", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
